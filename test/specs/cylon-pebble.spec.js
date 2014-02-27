cc81ea92a861b4cce4cfea1bf9b3c68a3594a7c4
deadprogram <ron@hybridgroup.com>
2014-02-26 16:32:08 -0800

    All tests green in the new CoffeeScript-less era

---
+++ test/specs/cylon-pebble.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var namespace = require('node-namespace'),
+    pebble = source("cylon-pebble");
+
+describe("Cylon.Pebble", function() {
+  it("can register the adaptor and driver", function() {
+    pebble.register.should.be.a('function');
+  });
+
+  it("can create adaptor", function() {
+    pebble.adaptor.should.be.a('function');
+    expect(pebble.adaptor()).to.be.a('object');
+  });
+
+  it("can create driver", function() {
+    pebble.driver.should.be.a('function');
+    expect(pebble.driver({ device: {} })).to.be.a('object');
+  });
+});
+
