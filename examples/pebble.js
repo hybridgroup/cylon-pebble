5d6abc48348c8a81640ccdf29ab8b4e2601cc2cc
Edgar O Silva <oswaldo.silva@gmail.com>
2014-03-26 10:24:59 -0500

    Fix bugs in pebble examples.

--- examples/pebble.js
+++ examples/pebble.js
@@ -1,42 +1,36 @@
-Cylon = require('cylon');
+var Cylon = require('cylon');
 
 Cylon.api({
   host: '0.0.0.0',
   port: '8080'
 });
 
-PebbleRobot = (function() {
+pebbleRobot = {
+  name: 'pebble',
 
-  function PebbleRobot() {}
-
-  PebbleRobot.prototype.connection = {
+  connection: {
     name: 'pebble',
     adaptor: 'pebble'
-  };
+  },
 
-  PebbleRobot.prototype.device = {
+  device: {
     name: 'pebble',
     driver: 'pebble'
-  };
+  },
 
-  PebbleRobot.prototype.work = function(self) {
-    var c;
-    c = 100;
-    return every(1..second(), function() {
-      var str;
-      c++;
-      str = "c: " + c;
-      self.pebble.message_queue().push(str);
-      return console.log(self.pebble.last_message());
-    });
-  };
+  work: function(my) {
+    //var c;
+    //c = 100;
+    //return every(1..second(), function() {
+      //var str;
+      //c++;
+      //str = "c: " + c;
+      //my.pebble.message_queue().push(str);
+      //return console.log(my.pebble.last_message());
+    //});
+  }
 
-  return PebbleRobot;
+}
 
-})();
-
-bot = new PebbleRobot();
-bot.name = 'pebble';
-Cylon.robot(bot);
-
+Cylon.robot(pebbleRobot);
 Cylon.start();
